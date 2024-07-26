41accc7066607dfa1574e8d8c6f9a41476a54886
Craig Jarvis <craig.jarvis@gmail.com>
2024-07-26 18:30:08 +1000

    Applied Rider formatting

--- Emby.Plugin.AppriseApi/Plugin.cs
+++ Emby.Plugin.AppriseApi/Plugin.cs
@@ -7,61 +7,60 @@ using MediaBrowser.Controller.Plugins;
 using MediaBrowser.Model.Drawing;
 using MediaBrowser.Model.Logging;
 
-namespace Emby.Plugin.AppriseApi
+namespace Emby.Plugin.AppriseApi;
+
+/// <summary>
+///     The plugin.
+/// </summary>
+public class Plugin : BasePluginSimpleUI<PluginOptions>, IHasThumbImage
 {
-    /// <summary>
-    /// The plugin.
-    /// </summary>
-    public class Plugin : BasePluginSimpleUI<PluginOptions>, IHasThumbImage
+    /// <summary>The Plugin ID.</summary>
+    private readonly Guid _id = new("9f19e7af-0cd0-4370-967c-3a87fb63a7d2"); // << Generate one: Tools >> Create GUID
+
+    private readonly ILogger _logger;
+
+    /// <summary>Initializes a new instance of the <see cref="Plugin" /> class.</summary>
+    /// <param name="applicationHost">The application host.</param>
+    /// <param name="httpClient">A HTTP Client</param>
+    /// <param name="logManager">The log manager.</param>
+    public Plugin(
+        IApplicationHost applicationHost,
+        IHttpClient httpClient,
+        ILogManager logManager) : base(applicationHost)
     {
-        /// <summary>The Plugin ID.</summary>
-        private readonly Guid _id = new Guid("9f19e7af-0cd0-4370-967c-3a87fb63a7d2"); // << Generate one: Tools >> Create GUID
+        _logger = logManager.GetLogger(Name);
+        _logger.Info("My plugin ({0}) is getting loaded", Name);
+        HttpClient = httpClient;
+    }
 
-        private readonly ILogger _logger;
+    /// <summary>Gets the description.</summary>
+    /// <value>The description.</value>
+    public override string Description => "Get notified about server events via an Apprise API instance";
 
-        /// <summary>Initializes a new instance of the <see cref="Plugin" /> class.</summary>
-        /// <param name="applicationHost">The application host.</param>
-        /// <param name="httpClient">A HTTP Client</param>
-        /// <param name="logManager">The log manager.</param>
-        public Plugin(
-            IApplicationHost applicationHost,
-            IHttpClient httpClient,
-            ILogManager logManager) : base(applicationHost)
-        {
-            _logger = logManager.GetLogger(Name);
-            _logger.Info("My plugin ({0}) is getting loaded", Name);
-            HttpClient = httpClient;
-        }
+    /// <summary>Gets the unique id.</summary>
+    /// <value>The unique id.</value>
+    public override Guid Id => _id;
 
-        /// <summary>Gets the description.</summary>
-        /// <value>The description.</value>
-        public override string Description => "Get notified about server events via an Apprise API instance";
+    public static IHttpClient HttpClient { get; set; }
 
-        /// <summary>Gets the unique id.</summary>
-        /// <value>The unique id.</value>
-        public override Guid Id => _id;
-        
-        public static IHttpClient HttpClient { get; set; }
+    /// <summary>Gets the name of the plugin</summary>
+    /// <value>The name.</value>
+    public sealed override string Name => "AppriseAPI Notifications";
 
-        /// <summary>Gets the name of the plugin</summary>
-        /// <value>The name.</value>
-        public sealed override string Name => "AppriseAPI Notifications";
+    /// <summary>Gets the thumb image format.</summary>
+    /// <value>The thumb image format.</value>
+    public ImageFormat ThumbImageFormat => ImageFormat.Png;
 
-        /// <summary>Gets the thumb image format.</summary>
-        /// <value>The thumb image format.</value>
-        public ImageFormat ThumbImageFormat => ImageFormat.Png;
+    /// <summary>Gets the thumb image.</summary>
+    /// <returns>An image stream.</returns>
+    public Stream GetThumbImage()
+    {
+        var type = GetType();
+        return type.Assembly.GetManifestResourceStream(type.Namespace + ".ThumbImage.png");
+    }
 
-        /// <summary>Gets the thumb image.</summary>
-        /// <returns>An image stream.</returns>
-        public Stream GetThumbImage()
-        {
-            var type = GetType();
-            return type.Assembly.GetManifestResourceStream(type.Namespace + ".ThumbImage.png");
-        }
-
-        protected override void OnOptionsSaved(PluginOptions options)
-        {
-            _logger.Info("My plugin ({0}) options have been updated.", Name);
-        }
+    protected override void OnOptionsSaved(PluginOptions options)
+    {
+        _logger.Info("My plugin ({0}) options have been updated.", Name);
     }
 }
--- Emby.Plugin.AppriseApi/PluginOptions.cs
+++ Emby.Plugin.AppriseApi/PluginOptions.cs
@@ -7,42 +7,36 @@ using Emby.Web.GenericEdit.Validation;
 using MediaBrowser.Model.Attributes;
 using MediaBrowser.Model.Logging;
 
-namespace Emby.Plugin.AppriseApi
+namespace Emby.Plugin.AppriseApi;
+
+public class PluginOptions : EditableOptionsBase
 {
-    public class PluginOptions : EditableOptionsBase
+    public override string EditorTitle => "Plugin Options";
+
+    public override string EditorDescription => "Get notified about server events via an Apprise API instance.\n"
+                                                + "The options below are just a few examples for creating UI elements.";
+
+    [DisplayName("Output Folder")]
+    [Description("Please choose a folder for plugin output")]
+    [EditFolderPicker]
+    public string TargetFolder { get; set; }
+
+    [Description("The log level determines how messages will be logged")]
+    public LogSeverity LogLevel { get; set; }
+
+    [Description("Apprise server URL, including http(s):// and port if needed")]
+    [Required]
+    public string AppriseServerURL { get; set; }
+
+    [Description("Configuration Key from Apprise API Configuration Manager")]
+    public string AppriseConfigurationKey { get; set; }
+
+    protected override void Validate(ValidationContext context)
     {
-        public override string EditorTitle => "Plugin Options";
+        var validateUrl = Uri.TryCreate(AppriseServerURL, UriKind.Absolute, out var uriResult);
+        if (!validateUrl) context.AddValidationError(nameof(AppriseServerURL), "Provided URL is not valid");
 
-        public override string EditorDescription => "Get notified about server events via an Apprise API instance.\n"
-                                                    + "The options below are just a few examples for creating UI elements.";
-
-        [DisplayName("Output Folder")]
-        [Description("Please choose a folder for plugin output")]
-        [EditFolderPicker]
-        public string TargetFolder { get; set; }
-
-        [Description("The log level determines how messages will be logged")]
-        public LogSeverity LogLevel { get; set; }
-
-        [Description("Apprise server URL, including http(s):// and port if needed")]
-        [Required]
-        public string AppriseServerURL { get; set; }
-        
-        [Description("Configuration Key from Apprise API Configuration Manager")]
-        public string AppriseConfigurationKey { get; set; }
-
-        protected override void Validate(ValidationContext context)
-        {
-            var validateUrl = Uri.TryCreate(AppriseServerURL, UriKind.Absolute, out var uriResult);
-            if (!validateUrl)
-            {
-                context.AddValidationError(nameof(AppriseServerURL), "Provided URL is not valid");
-            }
-
-            if (uriResult.Scheme != Uri.UriSchemeHttp || uriResult.Scheme != Uri.UriSchemeHttps)
-            {
-                context.AddValidationError(nameof(AppriseServerURL), "URL scheme is not valid. Must be HTTP or HTTPS");
-            }
-        }
+        if (uriResult.Scheme != Uri.UriSchemeHttp || uriResult.Scheme != Uri.UriSchemeHttps)
+            context.AddValidationError(nameof(AppriseServerURL), "URL scheme is not valid. Must be HTTP or HTTPS");
     }
 }
